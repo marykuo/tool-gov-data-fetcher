438529ddb82cb86cfa72253a7bf07f4bb13749b1
marykuo <meiyu19980928@gmail.com>
2026-02-04 00:45:53 +0800

    feat: error proofing when saving file

--- src/gov_data_fetcher/core/utility.py
+++ src/gov_data_fetcher/core/utility.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 
@@ -9,5 +10,6 @@ def fetch_api(url):
 
 
 def save_json_to_file(data, filename):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
